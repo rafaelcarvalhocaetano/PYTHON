b78196d4190d1f851a27cbdc448652d1535097fb
Rafael Carvalho <rapha.pse@outlook.com>
2016-12-02 20:09:05 -0200

    Recaptulando projetos python - foco desenvolvimento de aplicações comerciais com KIVY

--- Python_25_AtribuicaoMultiplaII/atrbuicao_multipla.py
+++ Python_25_AtribuicaoMultiplaII/atrbuicao_multipla.py
@@ -1,4 +1,4 @@
-#Isso em java
+"""#Isso em java
 a = 1
 b = 5
 x = a
@@ -14,3 +14,11 @@ y = 20
 x, y = y, x
 
 print(x, y)
+"""
+a, b, c = 2, 3,4
+a,b,c = a*2, a+b+c, a*b*c
+print(a,b,c)
+
+nome, sobrenome = "rafael", "carvalho"
+print(nome, sobrenome)
+print(sobrenome)
