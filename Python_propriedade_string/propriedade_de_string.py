347ed2acb19bf9066e3e7c7fedd48324a559633a
Rafael Carvalho <rapha.pse@outlook.com>
2016-12-06 14:20:36 -0200

    Recaptulando projetos python - foco desenvolvimento de aplicações comerciais com KIVY

--- Python_propriedade_string/propriedade_de_string.py
+++ Python_propriedade_string/propriedade_de_string.py
@@ -2,3 +2,8 @@ s = "Rafael Carvalho Caetano"
 print(len(s))
 print(type(s))
 print(s[5]) #indice 5 == L
+s.split(" ")
+print(s.split(" "))
+print(s.replace("ra", ""))
+print(id(s))
+print(id(s.replace("de", "")))
