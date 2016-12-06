73485936702a2b8eaf97436ac8b325c92a0e64ad
Rafael Carvalho <rapha.pse@outlook.com>
2016-12-06 14:29:39 -0200

    Recaptulando projetos python - foco desenvolvimento de aplicações comerciais com KIVY

---
+++ Python_52_alterando_string/alterando_string.py
@@ -0,0 +1,12 @@
+#EXEMPLO 1
+s = 'Alterando String'
+for item in s:
+    print(item)
+
+#EXEMPLO 2
+print()
+sa = 'alterando string'
+indice = 0
+while (indice < len(sa)):
+    print(indice," - ", s[indice])
+    indice += 1
