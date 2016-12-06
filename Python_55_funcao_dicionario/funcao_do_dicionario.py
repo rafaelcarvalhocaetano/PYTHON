6d37158629083ffc4ee61fb87946546192ff3457
Rafael Carvalho <rapha.pse@outlook.com>
2016-12-06 15:57:12 -0200

    Recaptulando projetos python - foco desenvolvimento de aplicações comerciais com KIVY

---
+++ Python_55_funcao_dicionario/funcao_do_dicionario.py
@@ -0,0 +1,25 @@
+tel = {}
+tel = {}
+tel = {
+    1111222222: "rafael",
+    2222333333: "rose",
+    3333444444: "vitor",
+    4444555555: "Heitor"
+}
+print(tel)
+print(len(tel))
+print(tel.keys()) #retorna a lista com os elementos
+print(tel.values()) #retorna a lista com os valores
+print(tel[1111222222])
+print(tel.get(1111222222))
+print(tel.popitem())
+print(tel.popitem())
+tel = {}
+tel = {}
+tel = {
+    1111222222: "rafael",
+    2222333333: "rose",
+    3333444444: "vitor",
+    4444555555: "Heitor"
+}
+print(1111222222 in tel)
