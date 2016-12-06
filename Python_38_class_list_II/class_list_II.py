d955e6c06709d228909a2255e7c08290b8646d31
Rafael Carvalho <rapha.pse@outlook.com>
2016-12-05 22:05:11 -0200

    Recaptulando projetos python - foco desenvolvimento de aplicações comerciais com KIVY

---
+++ Python_38_class_list_II/class_list_II.py
@@ -0,0 +1,14 @@
+lista = [1,2,3,4]
+print(lista)
+
+lista = ['a', 3]
+print(lista)
+
+ls = [['a', 'b', 'c'], [5,8,2], []]
+print(ls)
+print(ls[0])
+print(ls [1])
+print(ls [2])
+print(ls[0][0])
+print(ls[0][1])
+print(ls[0][2])
