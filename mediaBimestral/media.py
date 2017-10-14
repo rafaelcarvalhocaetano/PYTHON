6669112bc36c19e111b704a9ebfa33c6e0151e48
Rafael <rapha.pse@outlook.com>
2017-10-13 21:02:34 -0300

    programa conversor métros para centimétros

--- mediaBimestral/media.py
+++ mediaBimestral/media.py
@@ -5,13 +5,13 @@ nota4 = float(input("Informe a 4° nota : "))
 
 media = (nota1+nota2+nota3+nota4) / 4
 
-print("A média do Bimestre foi : {} ".format(media))
+print("A média do Bimestre foi : {:.1f} ".format(media))
 
 if media >= 10.0:
-    print("Parabéns sua média foi {}, você passou !!! ".format(media))
+    print("Parabéns sua média foi {:.1f}, você passou !!! ".format(media))
 if (media >= 6.0 and media <= 9.0):
-    print("Parabéns sua média foi {}, passou raspando !!! ".format(media))
+    print("Parabéns sua média foi {:.1f}, passou raspando !!! ".format(media))
 if media <= 5.0:
-    print("Reprovado sua média foi {} ".format(media))
+    print("Reprovado sua média foi {:.1f} ".format(media))
 
 
